d85190ac283a3e7ddece4ab16bd018154ecee6de
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-22 10:05:51 +0100

    created cinema model

---
+++ MovieWeb/Models/Cinema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieWeb.Models
+{
+	public class Actor
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string ProfilePictureURL { get; set; }
+
+		public string FullName { get; set; }
+
+		public string Bio { get; set; }
+
+	}
+}
+
