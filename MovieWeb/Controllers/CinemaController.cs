29f214d72f276a316b3ba05fc8d0ff8b040d6acd
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-23 10:54:52 +0100

    Added cinema controller

---
+++ MovieWeb/Controllers/CinemaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieWeb.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieWeb.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CinemaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
+
