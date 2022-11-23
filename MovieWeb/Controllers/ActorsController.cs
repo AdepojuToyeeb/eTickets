ee3bec60ee063e2a16ed30aad34e3ff75574a305
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-23 10:15:41 +0100

    Added Actors Controller

---
+++ MovieWeb/Controllers/ActorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieWeb.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
+
