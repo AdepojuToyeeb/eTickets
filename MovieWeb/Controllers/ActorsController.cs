fadf8965ce2385015967c97fc92625066b6e6ade
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-23 11:51:50 +0100

    Added Actor List View

--- MovieWeb/Controllers/ActorsController.cs
+++ MovieWeb/Controllers/ActorsController.cs
@@ -21,7 +21,7 @@ namespace MovieWeb.Controllers
         public IActionResult Index()
         {
             var data = _context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
