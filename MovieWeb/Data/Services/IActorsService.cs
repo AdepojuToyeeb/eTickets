2574c56e4e9fa5cf0a42bbc3fc3c88a3f577be8d
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-24 10:53:08 +0100

    Added Actor Service

---
+++ MovieWeb/Data/Services/IActorsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MovieWeb.Models;
+
+namespace MovieWeb.Data.Services
+{
+	public interface IActorsService
+	{
+		IEnumerable<Actor> GetAll();
+
+		Actor GetById(int Id);
+
+		void Add(Actor actor);
+
+		Actor Update(int Id, Actor newActor);
+
+		void Delete(int Id);
+	}
+}
+
