59af797c592d09beb363716750c9e6cf42bbc767
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-11-24 16:46:04 +0100

    added base repository class

--- MovieWeb/Data/Services/IActorsService.cs
+++ MovieWeb/Data/Services/IActorsService.cs
@@ -5,15 +5,7 @@ namespace MovieWeb.Data.Services
 {
 	public interface IActorsService
 	{
-		Task<IEnumerable<Actor>> GetAllAsync();
-
-		Task<Actor> GetByIdAsync(int Id);
-
-		Task AddAsync(Actor actor);
-
-		Task<Actor> UpdateAsync(int Id, Actor newActor);
-
-		Task DeleteAsync(int Id);
+		
 	}
 }
 
